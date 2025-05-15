0c5a6fb7a0742d14838ef48e641082afdebe9bc5
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-15 17:12:06 +0100

    ng g @angular-architects/native-federation:init

---
+++ federation.config.js
@@ -0,0 +1,26 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config')
+
+// noinspection JSCheckFunctionSignatures
+module.exports = withNativeFederation({
+
+  name: 'exposing',
+
+  exposes: {
+    './Component': './src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+})
