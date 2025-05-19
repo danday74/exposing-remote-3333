2c1feb814f7fcd6ad60f876796a71c6161cc80f1
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-19 16:43:23 +0100

    fixed example

--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core'
+import { MyRemoteComponent } from 'my-shared-lib'
 
 @Component({
   selector: 'app-example',
-  imports: [],
+  imports: [MyRemoteComponent],
   templateUrl: './example.component.html',
   styleUrl: './example.component.scss',
 })
