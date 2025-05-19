fd5580a54f6f9a406e13228810728ca4d2e37df0
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-19 21:57:50 +0100

    my remote 3333

--- projects/my-shared-lib/src/lib/components/my-remote/my-remote-3333.component.spec.ts
+++ projects/my-shared-lib/src/lib/components/my-remote/my-remote-3333.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { MyRemoteComponent } from './my-remote.component'
+import { MyRemote3333Component } from './my-remote-3333.component'
 
-describe('MyRemoteComponent', () => {
-  let component: MyRemoteComponent
-  let fixture: ComponentFixture<MyRemoteComponent>
+describe('MyRemote3333Component', () => {
+  let component: MyRemote3333Component
+  let fixture: ComponentFixture<MyRemote3333Component>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyRemoteComponent],
+      imports: [MyRemote3333Component],
     })
       .compileComponents()
 
-    fixture = TestBed.createComponent(MyRemoteComponent)
+    fixture = TestBed.createComponent(MyRemote3333Component)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
--- projects/my-shared-lib/src/lib/components/my-remote/my-remote-3333.component.ts
+++ projects/my-shared-lib/src/lib/components/my-remote/my-remote-3333.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
-import { MyRemoteChildComponent } from './my-remote-child/my-remote-child.component'
+import { MyRemoteChild3333Component } from './my-remote-child/my-remote-child-3333.component'
 
 @Component({
-  selector: 'lib-my-remote',
-  imports: [MyRemoteChildComponent],
-  templateUrl: './my-remote.component.html',
-  styleUrl: './my-remote.component.scss',
+  selector: 'lib-my-remote-3333',
+  imports: [MyRemoteChild3333Component],
+  templateUrl: './my-remote-3333.component.html',
+  styleUrl: './my-remote-3333.component.scss',
 })
-export class MyRemoteComponent {}
+export class MyRemote3333Component {}
