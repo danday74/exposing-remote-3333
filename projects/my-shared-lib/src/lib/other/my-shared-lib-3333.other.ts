69c8e475c5db4a95fea4186c70d84fd4d37aa013
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-20 16:32:47 +0100

    load-3333-from-2222-inside-shell

--- projects/my-shared-lib/src/lib/other/my-shared-lib-3333.other.ts
+++ projects/my-shared-lib/src/lib/other/my-shared-lib-3333.other.ts
@@ -4,6 +4,7 @@ export const remoteObj3333A = { a: 1 }
 // noinspection JSUnusedGlobalSymbols
 export const remoteObj3333B = { b: 2 }
 
+// consuming host cannot see this since interfaces are a compile time only construct
 export interface ModMyRemote3333Service {
-  hello: () => void
+  hello: (msg?: string | null) => void
 }
--- projects/my-shared-lib/src/lib/services/my-remote-3333/my-remote-3333.service.ts
+++ projects/my-shared-lib/src/lib/services/my-remote-3333/my-remote-3333.service.ts
@@ -3,8 +3,11 @@ import { ModMyRemote3333Service } from '../../other/my-shared-lib-3333.other'
 
 @Injectable({ providedIn: 'root' })
 export class MyRemote3333Service implements ModMyRemote3333Service {
-  // noinspection JSUnusedGlobalSymbols
-  hello() {
-    console.log('hello MyRemote3333Service')
+  hello(msg: string | null = null) {
+    if (msg) {
+      console.log('hello MyRemote3333Service', msg)
+    } else {
+      console.log('hello MyRemote3333Service')
+    }
   }
 }
